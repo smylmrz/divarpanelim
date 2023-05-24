14081fa5c82564f443c20a819b5482859bbe3db5
Ismayil Mirzayev <smylmrz@gmail.com>
2023-05-25 02:53:24 +0400

    updated gitignore

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -12,7 +12,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 <div id="app">
@@ -22,7 +22,5 @@
         @yield('content')
     </main>
 </div>
-
-@vite('resources/js/app.js')
 </body>
 </html>
