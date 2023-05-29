68a57766d762072cdfa7887916f93a7b1f194de7
Ismayil Mirzayev <smylmrz@gmail.com>
2023-05-29 17:34:06 +0400

    slider place holder

---
+++ resources/js/components/Slider/Placeholder.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="animate-pulse">
+        <div class="space-y-10 pt-20 mb-10">
+            <div class="space-y-2">
+                <div class="h-16 bg-neutral-200 rounded-md w-2/3"></div>
+                <div class="h-16 bg-neutral-200 rounded-md"></div>
+                <div class="h-16 bg-neutral-200 rounded-md w-1/2"></div>
+            </div>
+            <div class="space-y-2">
+                <div class="h-5 bg-neutral-200 rounded-md"></div>
+                <div class="h-5 bg-neutral-200 rounded-md"></div>
+                <div class="h-5 bg-neutral-200 rounded-md w-20"></div>
+            </div>
+        </div>
+        <div class="mt-10 flex items-center gap-10">
+            <div class="rounded-full bg-neutral-200 w-80 h-16"></div>
+            <div class="rounded-full bg-neutral-200 w-80 h-16"></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
